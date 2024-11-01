7fb4e030f343bd24cc463991092716b554183227
changu.lee <cksrn5418@konkuk.ac.kr>
2024-11-01 18:23:58 +0900

    작업 내용 갱신

--- Assets/Scripts/InGame/MiniGames/CuttingFruit/CuttingFruitTarget.cs
+++ Assets/Scripts/InGame/MiniGames/CuttingFruit/CuttingFruitTarget.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class CuttingFruitTarget : MonoBehaviour
+namespace PlayJam.InGame.CuttingFruit
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class CuttingFruitTarget : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        private Rigidbody2D[] _fragments;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField]
+        private float _weight;
+
+        [SerializeField]
+        private float _gravity;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Initialize()
+        {
+            Array.ForEach(_fragments, (x) =>
+            {
+                x.velocity = Vector3.zero;
+                x.mass = _weight;
+                x.gravityScale = _gravity;
+                x.transform.localPosition = Vector3.zero;
+                x.bodyType = RigidbodyType2D.Static;
+            });
+
+
+        }
     }
 }
